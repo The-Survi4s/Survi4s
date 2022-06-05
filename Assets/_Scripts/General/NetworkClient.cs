d34a80f41311681dc287ced7fcf958c85c06af0f
Aryazaky <aryazakyazif@gmail.com>
2022-06-05 22:50:47 +0700

    Add an option to skip waiting for server. Untested

--- Assets/_Scripts/General/NetworkClient.cs
+++ Assets/_Scripts/General/NetworkClient.cs
@@ -16,6 +16,7 @@ public class NetworkClient : MonoBehaviour
     [SerializeField] private string IpAddress;
     private IPAddress ipAd;
     [SerializeField, Min(0)] private int _maximumRetries = 1;
+    [SerializeField] private bool _receiveSelfData = true;
 
     // Encryption ---------------------------------------------------------------------
     private RsaEncryption rsaEncryption;
@@ -28,7 +29,7 @@ public class NetworkClient : MonoBehaviour
     public bool isMaster { get; private set; }
     public int playersCount { get; private set; }
 
-    [SerializeField, Min(0.1f)] private float CheckTime = 0.8f;
+    [SerializeField, Min(0.1f)] private float CheckTime = 1f;
     private float _checkTime;
 
     // Connection status --------------------------------------------------------------
@@ -187,7 +188,8 @@ public class NetworkClient : MonoBehaviour
         UpWpn,
         PlVl,
         PJmp,
-        ChNm
+        ChNm,
+        GmOv
     }
 
     // Receive and Process incoming message here ----------------------------------
@@ -352,6 +354,11 @@ public class NetworkClient : MonoBehaviour
                     UnitManager.Instance.GetPlayer(info[0]).movement.Jump();
                     break;
                 }
+                case Header.GmOv:
+                {
+                    GameManager.Instance.ChangeState(GameManager.GameState.GameOver);
+                    break;
+                }
             }
         }
     }
@@ -396,8 +403,6 @@ public class NetworkClient : MonoBehaviour
         return genId;
     }
 
-    #region Send Message To Server
-
     // Private Method ---------------------------------------------------------------
     private void OnCreatedRoom(string roomName)
     {
@@ -420,6 +425,8 @@ public class NetworkClient : MonoBehaviour
         // Debugging
         Debug.Log("Room Joined");
     }
+
+    #region Send Message To Server
 
     // Public method that can be called to send message to server -------------------
     public void ChangeName(string newId, string newName)
@@ -473,38 +480,38 @@ public class NetworkClient : MonoBehaviour
     public void SetPlayerVelocity(Vector2 velocity, PlayerMovement.Axis axis)
     {
         string[] msg = { Header.PlVl.ToString(), velocity.x.ToString("f2"), velocity.y.ToString("f2"), axis.ToString() };
-        SendMessageClient("1", msg);
+        SendMessageClient(_receiveSelfData ? "1" : SelfRun(msg), msg);
     }
 
     public void SendMousePos(float x, float y)
     {
         string[] msg = {Header.MPos.ToString(), x.ToString("f2"), y.ToString("f2")};
-        SendMessageClient("1", msg);
+        SendMessageClient(_receiveSelfData ? "1" : SelfRun(msg), msg);
     }
 
     public void EquipWeapon(string weapon)
     {
         string[] msg = {Header.EqWp.ToString(), weapon};
-        SendMessageClient("1", msg);
+        SendMessageClient(_receiveSelfData ? "1" : SelfRun(msg), msg);
     }
 
     public void StartAttackAnimation() 
     {
         string[] msg = {Header.PAtk.ToString()};
-        SendMessageClient("1", msg);
+        SendMessageClient(_receiveSelfData ? "1" : SelfRun(msg), msg);
     }
 
     public void Jump()
     {
         string[] msg = { Header.PJmp.ToString() };
-        SendMessageClient("1", msg);
+        SendMessageClient(_receiveSelfData ? "1" : SelfRun(msg), msg);
     }
 
     public void StartMonsterAttackAnimation(int targetId) 
     {
         //Debug.Log("Monster Attack Animation");
         string[] msg = {Header.MAtk.ToString(), targetId.ToString()};
-        SendMessageClient("1", msg);
+        SendMessageClient(_receiveSelfData ? "1" : SelfRun(msg), msg);
     }
 
     public void SpawnBullet(Vector2 spawnPos, Vector2 mousePos, int spawnedByMonsterId = -1)
@@ -514,62 +521,62 @@ public class NetworkClient : MonoBehaviour
             Header.SwBl.ToString(), spawnPos.x.ToString("f2"), spawnPos.y.ToString("f2"), mousePos.x.ToString("f2"),
             mousePos.y.ToString("f2"), spawnedByMonsterId.ToString()
         };
-        SendMessageClient("1", msg);
+        SendMessageClient(_receiveSelfData ? "1" : SelfRun(msg), msg);
     }
 
     public void DestroyBullet(int id)
     {
         string[] msg = {Header.DBl.ToString(), id.ToString()};
-        SendMessageClient("1", msg);
+        SendMessageClient(_receiveSelfData ? "1" : SelfRun(msg), msg);
     }
 
     public void ModifyMonsterHp(int id, float amount)
     {
         string[] msg = {Header.MdMo.ToString(), id.ToString(), amount.ToString("f2")};
-        SendMessageClient("1", msg);
+        SendMessageClient(_receiveSelfData ? "1" : SelfRun(msg), msg);
     }
 
     public void ApplyStatusEffectToMonster(int targetId, StatusEffect effect, float duration, int strength)
     {
         string[] msg = {Header.MoEf.ToString(), targetId.ToString(), effect.ToString(), duration.ToString("f2"), strength.ToString() };
-        SendMessageClient("1", msg);
+        SendMessageClient(_receiveSelfData ? "1" : SelfRun(msg), msg);
     }
 
     public void ModifyPlayerHp(string playerName, float amount)
     {
         //Debug.Log("Send: Monster deals"+amount+" damage to "+playerName);
         string[] msg = {Header.MdPl.ToString(), playerName, amount.ToString("f2")};
-        SendMessageClient("1", msg);
+        SendMessageClient(_receiveSelfData ? "1" : SelfRun(msg), msg);
     }
 
     public void CorrectPlayerDeadPosition(float xPos, float yPos)
     {
         string[] msg = {Header.PlDd.ToString(), xPos.ToString("f2"), yPos.ToString("f2")};
-        SendMessageClient("1", msg);
+        SendMessageClient(_receiveSelfData ? "1" : SelfRun(msg), msg);
     }
 
     public void ModifyWallHp(int id, float amount)
     {
         string[] msg = {Header.MdWl.ToString(), id.ToString(), amount.ToString("f2")};
-        SendMessageClient("1", msg);
+        SendMessageClient(_receiveSelfData ? "1" : SelfRun(msg), msg);
     }
 
     public void ModifyStatueHp(float amount)
     {
         string[] msg = {Header.MdSt.ToString(), amount.ToString("f2")};
-        SendMessageClient("1", msg);
+        SendMessageClient(_receiveSelfData ? "1" : SelfRun(msg), msg);
     }
 
     public void RebuildWall(int brokenWallId, int amount)
     {
         string[] msg = { Header.RbWl.ToString(), brokenWallId.ToString(), amount.ToString() };
-        SendMessageClient("1", msg);
+        SendMessageClient(_receiveSelfData ? "1" : SelfRun(msg), msg);
     }
 
     public void UpgradeWeapon(string weaponName)
     {
         string[] msg = { Header.UpWpn.ToString(), weaponName };
-        SendMessageClient("1", msg);
+        SendMessageClient(_receiveSelfData ? "1" : SelfRun(msg), msg);
     }
 
     #endregion
@@ -586,5 +593,11 @@ public class NetworkClient : MonoBehaviour
         return int.Parse(idAndName.Substring(0, IdLength));
     }
 
+    private string SelfRun(string[] msg)
+    {
+        string data = msg.Aggregate(myId + myName, (current, x) => current + ("|" + x));
+        ReceiveMessage(data);
+        return "3";
+    }
     #endregion
 }
